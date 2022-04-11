e86d357f21df8f734f5b564e954eb99f5b47ba32
Bhargav Kantheti <bhargavkantheti5@gmail.com>
2022-04-11 15:10:50 +0550

    Fix epoch bug

--- CNN/ModelTraining.py
+++ CNN/ModelTraining.py
@@ -7,7 +7,7 @@ class ModelTraining:
 
     def train(self, xtrain, ytrain, xtest, ytest, epochs=5):
         self.model.fit(xtrain, ytrain, validation_data=(
-            xtest, ytest), epochs=10)
+            xtest, ytest), epochs=epochs)
 
     def save(self):
         self.model.save('../models/tf-model')
